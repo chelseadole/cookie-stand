0f420a3b77fe4dd1f7b16a9082fd148c33900284
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-14 15:51:17 -0700

    successfully created simulatedCookiesDayArray fn, which adds to empty cookie array

--- app.js
+++ app.js
@@ -4,42 +4,78 @@ var pikeLoc = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
-  cookiesPerHour: (custNumGen(pikeLoc.minCust, pikeLoc.maxCust) * avgCookies),
-  cookiesForDayArr: [],
+  custNumGen: function(minCust, maxCust) {
+    return Math.floor(Math.random() * (maxCust - minCust)) + 1;
+  },
+  pikeCookiesDayArr: [],
+  storeTotal: 0,
+  simulatedCookiesDayArray: function(minCust, maxCust, avgCookies) {
+    for (var i = 0; i <= 13; i++) {
+      this.pikeCookieDayArr.push(this.custNumGen(this.minCust, this.maxCust) * this.avgCookies);
+    }
+  }
 };
 
 var seatacLoc = {
   minCust: 3,
   maxCust: 24,
   avgCookies: 1.2,
-  cookiesPerHour: (custNumGen(seatacLoc.minCust, seatacLoc.maxCust) * avgCookies),
-  cookiesForDayArr: [],
+  custNumGen: function(minCust, maxCust) {
+    return Math.floor(Math.random() * (maxCust - minCust)) + 1;
+  },
+  seatacCookieDayArr: [],
+  storeTotal: 0,
+  simulatedCookiesDayArray: function(minCust, maxCust, avgCookies) {
+    for (var i = 0; i <= 13; i++) {
+      this.seatacCookieDayArr.push(this.custNumGen(this.minCust, this.maxCust) * this.avgCookies);
+    }
+  }
 };
 
 var centerLoc = {
   minCust: 11,
   maxCust: 38,
   avgCookies: 3.7,
-  cookiesPerHour: (custNumGen(centerLoc.minCust, centerLoc.maxCust) * avgCookies),
-  cookiesForDayArr: [],
+  custNumGen: function(minCust, maxCust) {
+    return Math.floor(Math.random() * (maxCust - minCust)) + 1;
+  },
+  centerCookieDayArr: [],
+  storeTotal: 0,
+  simulatedCookiesDayArray: function(minCust, maxCust, avgCookies) {
+    for (var i = 0; i <= 13; i++) {
+      this.centerCookieDayArr.push(this.custNumGen(this.minCust, this.maxCust) * this.avgCookies);
+    }
+  }
 };
 
 var capitolLoc = {
   minCust: 20,
   maxCust: 38,
   avgCookies: 2.3,
-  cookiesPerHour: (custNumGen(capitolLoc.minCust, capitolLoc.maxCust) * avgCookies),
-  cookiesForDayArr: [],
+  custNumGen: function(minCust, maxCust) {
+    return Math.floor(Math.random() * (maxCust - minCust)) + 1;
+  },
+  capitolCookieDayArr: [],
+  storeTotal: 0,
+  simulatedCookiesDayArray: function(minCust, maxCust, avgCookies) {
+    for (var i = 0; i <= 13; i++) {
+      this.capitolCookieDayArr.push(this.custNumGen(this.minCust, this.maxCust) * this.avgCookies);
+    }
+  }
 };
 
 var alkiLoc = {
   minCust: 2,
   maxCust: 16,
   avgCookies: 4.6,
-  cookiesPerHour: (custNumGen(alkiLoc.minCust, alkiLoc.maxCust) * avgCookies),
-  cookiesForDayArr: [],
-};
-
-custNumGen: function(minCust, maxCust) {
+  custNumGen: function(minCust, maxCust) {
     return Math.floor(Math.random() * (maxCust - minCust)) + 1;
-},
+  },
+  alkiCookieDayArr: [],
+  storeTotal: 0,
+  simulatedCookiesDayArray: function(minCust, maxCust, avgCookies) {
+    for (var i = 0; i <= 13; i++) {
+      this.alkiCookieDayArr.push(this.custNumGen(this.minCust, this.maxCust) * this.avgCookies);
+    }
+  }
+};
