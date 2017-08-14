f78878367027fc841ffe761a4278b83eb2b14059
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-14 14:48:14 -0700

    wrote cookiesPerHour, to calc and store a simulation of cookies per hour at each store

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 'use strict';
 
 var pikeLoc = {
-    minCust: 23,
-    maxCust: 65,
-    avgCookies: 6.3
-  },
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  cookiesPerHour: (custNumGen(pikeLoc.minCust, pikeLoc.maxCust) * avgCookies),
+},
 
-genRandom: function(minCust, maxCust) {
+var seatacLoc = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  cookiesPerHour: (custNumGen(seatacLoc.minCust, seatacLoc.maxCust) * avgCookies),
+},
+
+var centerLoc = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  cookiesPerHour: (custNumGen(centerLoc.minCust, centerLoc.maxCust) * avgCookies),
+},
+
+var capitolLoc = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  cookiesPerHour: (custNumGen(capitolLoc.minCust, capitolLoc.maxCust) * avgCookies),
+},
+
+var alkiLoc = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  cookiesPerHour: (custNumGen(alkiLoc.minCust, alkiLoc.maxCust) * avgCookies),
+},
+
+custNumGen: function(minCust, maxCust) {
     return Math.floor(Math.random() * (maxCust - minCust)) + 1;
-}
+},
